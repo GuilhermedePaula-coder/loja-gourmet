c20fcd8a5caacbb61c9abeef5e7df584336fb56f
Enzo Ferreira <enzocafaria@gmail.com>
2025-09-24 11:55:53 -0300

    adicionando main e app

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+import Navegacao from "./components/Navegacao"
+import Destaque from "./components/Destaque"
+import Beneficios from "./components/Beneficios"
+import GradePratos from "./components/GradePratos"
+import Depoimentos from "./components/Depoimentos"
+import FormularioContato from "./components/FormularioContato"
+import Rodape from "./components/Rodape"
+
+function App() {
+  return (
+    <>
+      <Navegacao />
+      <Destaque />
+      <Beneficios />
+      <GradePratos />
+      <Depoimentos />
+      <FormularioContato />
+      <Rodape />
+    </>
+  )
+}
+
+export default App
